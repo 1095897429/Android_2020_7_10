17f76270f84345f21115414899f8eeda86da0a78
周亮 <1095897429@qq.com>
2020-07-13 11:35:07 +0800

    bug文件，已修复

--- app/src/main/java/com/liangzai/myapplication/MainActivity.kt
+++ app/src/main/java/com/liangzai/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.liangzai.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("tag","这里有bug，已修复")
     }
 }
